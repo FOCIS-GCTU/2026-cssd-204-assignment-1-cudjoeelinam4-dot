bcc2ced1bcb03ca6d62f8f9e67bbf80ed5857c5d
cudjoeelinam4-dot <cudjoeelinam4@gmail.com>
2026-05-30 11:38:55 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
 
-def main():
-  pass #code goes here
+# File: <initials.py>
+# Description: <Print my stylized initials>
+# Assignment Number: <Assignment Number, 2>
+#
+# Name: <ELINAM CUDJOE GBOLOGAN>
+# STUDENT ID:  <2425401168>
+# Email: <cudjoelinam4@gmail.com>
+# Grader: <AUGUSTUS BUCKMAN>
+#
+# On my honor, <ELINAM CUDJOE GBOLOGAN>, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+
+
+def main():
+    # Print my stylized initials
+
+    print()
+    print("...ECG")
+    print()
+
+    print("...EEEEEEEEEEEE........CCCCCCCCCCCC........GGGGGGGGGGGG.....")
+    print("...EEEEEEEEEEEE........CCCCCCCCCCCC........GGGGGGGGGGGG.....")
+    print("...EEEE................CCCC................GGG.........G.....")
+    print("...EEEE................CCCC................GGG.........G.....")
+    print("...EEEEEEEEEE..........CCCC................GGG..GGGGGGGG.....")
+    print("...EEEEEEEEEE..........CCCC................GGG..GGGGGGGG.....")
+    print("...EEEE................CCCC................GGG.......GGG.....")
+    print("...EEEE................CCCC................GGG.......GGG.....")
+    print("...EEEEEEEEEEEE...**...CCCCCCCCCCCC...**...GGGGGGGGGGGGG...**")
+    print("...EEEEEEEEEEEE...**...CCCCCCCCCCCC...**...GGGGGGGGGGGGG...**")
+    print()
 
 
 main()
